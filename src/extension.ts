bbac2dbb519176b130130b6289213f1ee2bee171
Klaudia <blazyczek.klaudia@gmail.com>
2023-07-24 14:45:56 +0200

    update dependencies and restructure project

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { createFolderWithFiles } from "commands";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const createFolderWithFilesCommand = vscode.commands.registerCommand(
+    "react-files-generator.createFolderWithFiles",
+    createFolderWithFiles
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "react-files-generator" is now active!');
+  const test = vscode.commands.registerCommand(
+    "react-files-generator.test",
+    () => {
+      vscode.window.showInformationMessage("Test");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('react-files-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from React Files Generator!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(createFolderWithFilesCommand);
+  context.subscriptions.push(test);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
